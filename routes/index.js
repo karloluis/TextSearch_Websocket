e5acab4710423deeafbe767ef403ea9f73dc8899
Karlo Luis Martinez Martos <karlo.luis.m@gmail.com>
2017-11-09 12:42:15 -0400

    Unable to add successful separators to show extended datalist values.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var expressWs = require('express-ws')(app)
+const _ = require('lodash');
 
 const Stocks = require('../models/Stocks');
 
@@ -12,21 +13,31 @@ router.get('/', function(req, res, next) {
 
 // Route to manage finding stocks by tickers
 router.ws('/', function(ws, req) {
-  ws.on('message', async function(message){
+  ws.on('message', async function(dataPack){
+    const dataPayload = JSON.parse(dataPack).payload;
+    const tickers = dataPayload || [];
     // Regex that finds a ticker according to user input.
     // ignores casing (ex. goog and GOOG are the same)
-    const ticks = new RegExp('^'+message, 'i');
-    const matchingStocks = await Stocks.find(
-      { Ticker: { $regex: ticks }},
-      {'Ticker': 1, 'Country': 1, 'Price': 1});
+
+    const matchingStocks = tickers.reduce( async function(stocks, ticker) {
+      if (ticker == '') {
+        return stocks;
+      }
+      const pattern = new RegExp('^'+ticker, 'i');
+      let match = await Stocks.find(
+        { Ticker: { $regex: pattern }},
+        {'Ticker': 1, 'Country': 1, 'Price': 1})
+        .exec();
+      return await _.concat(await stocks, await match);
+    }, []);
 
     // Wrap the resulting array of matched stocks to an object.
-    const data = {
-      payload: matchingStocks
+    const dataSend = {
+      payload: _.uniq(await matchingStocks),
     }
 
     // Pipe the data as a string through the socket.
-    ws.send(JSON.stringify(data));
+    ws.send(JSON.stringify(dataSend));
   });
 });
 
